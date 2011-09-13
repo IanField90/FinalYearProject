e7a7caef43091a23e79dc075da61efe9bb7dd08c
IanField90 <ian.field90@gmail.com>
2011-09-13 15:01:54 +0100

    Add base android and rails projects

---
+++ android/src/uk/ac/reading/ianfield/se3ip11/TeachReachActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.reading.ianfield.se3ip11;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TeachReachActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web::Application.config.secret_token = '6a2c32b5b96e061e8ae82d1c27d6f9eeed85e248f184f65494ee28fed20d3bb41781c5b53b3fdc58b1e33f783ea4c8f2faa51f761c8f2fd678ebe6a5e292966b'
