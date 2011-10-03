1c96bdc236a8bc2411dff37987f10f400401ad77
IanField90 <ian.field90@gmail.com>
2011-10-03 08:33:46 +0100

    Add admin column to users

--- web/app/controllers/sessions_controller.rb
+++ web/app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       else
         cookies[:auth_token] = user.auth_token
       end
-      redirect_to root_url, :flash => { :success => "Logged in!" } #:notice => "Logged in!"
+      redirect_to root_url #, :flash => { :success => "Logged in!" }
     else
       flash[:error] = "Invalid email or password"
       render "new"
--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110914164334) do
+ActiveRecord::Schema.define(:version => 20111003070414) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20110914164334) do
     t.string   "auth_token"
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
+    t.boolean  "admin",                  :default => false
   end
 
 end
