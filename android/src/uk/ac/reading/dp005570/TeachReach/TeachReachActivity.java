0b2670aaeed27cbbadacba3c064d27900ed1bda7
IanField90 <ian.field90@gmail.com>
2011-10-07 13:17:42 +0100

    Add more folders that were missing.

---
+++ android/src/uk/ac/reading/dp005570/TeachReach/TeachReachActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.reading.dp005570.TeachReach;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TeachReachActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
