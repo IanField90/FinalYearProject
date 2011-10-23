cabd9863939c6f9dd326370aa5efc503c5ac3a88
IanField90 <ian.field90@gmail.com>
2011-10-23 22:45:05 +0100

    Code tidyup

--- android/src/uk/ac/reading/dp005570/TeachReach/QuizActivity.java
+++ android/src/uk/ac/reading/dp005570/TeachReach/QuizActivity.java
@@ -27,66 +27,60 @@ public class QuizActivity extends Activity {
 		//Set up quiz
 		populateQuiz();
 		number_of_questions = quiz.size();
+		//We start on Question 1
 		questionNumber = 1;
-		
+
 		//Set up progress label
 		question_progress = (TextView) findViewById(R.id.question_progress);
 		question_progress.setText( questionNumber + " / " + number_of_questions);
-		
+
 		//Label question with "Question"
 		TextView question_title = (TextView) findViewById(R.id.question_title);
 		question_title.setText(R.string.question);
-		
+
 		// Actually prepare question
 		ll = (LinearLayout) findViewById(R.id.question_options);
 		loadQuestion(quiz.get(0));
-
-		questionNumber = 1; 
-		number_of_questions = quiz.size();
-
 		
-//		setContentView(R.layout.quiz);
-
-//		for(Question q : quiz){
-//			loadQuestion(q);
-//		}
-
-
-//		LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//		RadioGroup rg = (RadioGroup) findViewById(R.layout.option_multiple);
-//		RadioButton rb = (RadioButton) findViewById(R.layout.option_multiple_item);
-//
-//		for(String option : quiz.get(0).getOptions()){
-//			rb.setText(option);
-//			rg.addView(rb);
-//		}
-		// Like quiz list activity for each option = new radio button in group
-		// or spinner control for blanks, 1 for each blank - 1 spinner for each
-		// but if slider question, only add slider then - requires special logic based on number of options (on change listener)(setMax)
-//		View v = inflater.inflate(R.layout.option_multiple, null);
-//		LinearLayout insertPoint = (LinearLayout) findViewById(R.id.question_options);
-//		insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
-
 	}
-	
+
 	public void loadQuestion(Question q){
+		//Question text will not be different whatever type the question it is
 		TextView question_text = (TextView) findViewById(R.id.question_text);
-		//question_text.setText("blab ewa rewa  ewarewar ewa rewahiorpewa hiophfido ewaklcnakm eawocpmecdkmwe mkl pewa");
 		question_text.setText(q.getQuestionText());
-		ArrayList<String> options_list = q.getOptions();
-		
-		Spinner options = new Spinner(this);
-        ArrayAdapter<String> options_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, options_list);
-        options_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        options.setAdapter(options_adapter);
-		
-		ll.addView(options);
+
+		switch(q.getType()){
+		case MULTIPLE_CHOICE:
+			//Get options
+			ArrayList<String> options_list = q.getOptions();
+			Spinner options = new Spinner(this);
+			//Load options ready for spinner
+			ArrayAdapter<String> options_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, options_list);
+			//Put options into spinner drop down
+			options_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			options.setAdapter(options_adapter);
+			//TODO add OnItemSelectedListener
+			//Load spinner at the location of R.id.question_options
+			ll.addView(options);
+			break;
+		case BLANKS:
+			//TODO 
+			break;
+		case SLIDER:
+			//TODO Hashmap slider value + label value
+			//TODO add slider value change listener to update label
+//			LinearLayout option_slider = (LinearLayout) findViewById(R.layout.option_slider);
+//			TextView slider_value = (TextView) findViewById(R.id.slider_value);
+//			SeekBar bar = (SeekBar) findViewById(R.id.slider);			
+			break;
+		}
+
 	}
 
 	public void populateQuiz(){
 		quiz = new ArrayList<Question>();
 		//TODO Actual population later on
-		//TODO Add 1 question of each type
+		//TODO Add blanks and slider(seekbar) question
 		String questionText = "What colour is the sky?";
 		QuestionType type = QuestionType.MULTIPLE_CHOICE;
 		ArrayList<String> options = new ArrayList<String>();
