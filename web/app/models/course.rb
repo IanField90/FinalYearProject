b58c58f0155a09aaf528c20c2163ba49fdd6d9a5
Ian Field <ian.field90@gmail.com>
2011-12-09 17:40:07 +0000

    Add cascading delete for Courses

--- web/app/models/course.rb
+++ web/app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
-  has_many :programmes
+  has_many :programmes, :dependent => :delete_all # ON DELETE CASCADE
   
   attr_accessible :course_name_en, :course_name_fr, :course_name_es
   validates_presence_of :course_name_en, :on => :create
