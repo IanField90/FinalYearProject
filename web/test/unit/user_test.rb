33464833587d701ad04ebb1fbc36f0685f340506
Ian Field <ian.field90@gmail.com>
2011-12-27 11:26:27 +0000

    Add test for valid and invalid user login

--- web/test/unit/user_test.rb
+++ web/test/unit/user_test.rb
@@ -4,4 +4,15 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "valid password" do
+    user = User.find_by_email('test@example.com')
+    assert user.authenticate("Password")
+  end
+  
+  test "invalid password" do
+    user = User.find_by_email('test@example.com')
+    assert !user.authenticate("ewarewarewarpu")
+  end
+  
 end
