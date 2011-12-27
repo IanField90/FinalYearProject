c59987e0fd95ee8f6e4f1af7480974c65caf0489
Ian Field <ian.field90@gmail.com>
2011-12-27 11:32:05 +0000

    Add more unit tests for User model

--- web/test/unit/user_test.rb
+++ web/test/unit/user_test.rb
@@ -5,6 +5,30 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   
+  test "empty creation" do
+    user = User.new
+    assert !user.save
+  end
+  
+  test "password blank creation" do
+    user = User.new
+    user.email = "testing@test.com"
+    assert !user.save
+  end
+  
+  test "password confirmation blank" do
+    user = User.new
+    user.email = "testing@test.com"
+    user.password = "test"
+    user.password_confirmation = ""
+    assert !user.save
+  end
+  
+  test "empty password" do
+    user = User.find_by_email('test@example.com')
+    assert !user.authenticate("")
+  end
+    
   test "valid password" do
     user = User.find_by_email('test@example.com')
     assert user.authenticate("Password")
