a99167681d771b23fd76a1f8b02cac28f0b1f005
Ian Field <ian.field90@gmail.com>
2012-01-24 12:47:07 +0000

    Begin JSON parser for Server menu list, refs #3

--- android/src/uk/ac/reading/dp005570/TeachReach/net/ServerCommunicationHelper.java
+++ android/src/uk/ac/reading/dp005570/TeachReach/net/ServerCommunicationHelper.java
@@ -14,6 +14,7 @@ import org.apache.http.conn.HttpHostConnectException;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.ProgressDialog;
 import android.util.Log;
@@ -31,7 +32,31 @@ public class ServerCommunicationHelper {
 	private HttpResponse response;
 	private BufferedReader in;
 	
+	//Constants in case these fields change at a later date
+	
+	//Universal JSON response fields
 	private final String TAG = "SERVER_COMS";
+	private final String ID = "id";
+	private final String UPDATED_AT = "updated_at";
+	
+	//Courses JSON response fields
+	private final String COURSE_NAME_EN = "course_name_en";
+	private final String COURSE_NAME_FR = "course_name_fr";
+	private final String COURSE_NAME_ES = "course_name_es";
+	
+	//Programmes JSON response fields
+	private final String PROGRAMMES = "programmes";
+	private final String COURSE_ID = "course_id";
+	private final String PROGRAMME_NAME_EN = "programme_name_en";
+	private final String PROGRAMME_NAME_FR = "programme_name_fr";
+	private final String PROGRAMME_NAME_ES = "programme_name_es";
+	
+	//Parts JSON response fields
+	private final String PARTS = "parts";
+	private final String PROGRAMME_ID = "programme_id";
+	private final String PART_NAME_EN = "part_name_en";
+	private final String PART_NAME_FR = "part_name_fr";
+	private final String PART_NAME_ES = "part_name_es";
 	
 	//TODO This does not work...
 	//Resources.getSystem().getString(uk.ac.reading.dp005570.TeachReach.R.string.server_uri);
@@ -44,11 +69,77 @@ public class ServerCommunicationHelper {
 	}
 
 	private void parseCourses(JSONArray list){
-		list.toString();
+		JSONObject course;
+		int id;
+		String en, fr, es, updated_at;
+		for (int i = 0; i < list.length()-1; i++){
+			try {
+				course = list.getJSONObject(i);
+				id = (Integer) course.get(ID);				
+				updated_at = course.get(UPDATED_AT).toString();
+				en = course.getString(COURSE_NAME_EN);
+				fr = course.getString(COURSE_NAME_FR);
+				es = course.getString(COURSE_NAME_ES);
+				
+				Log.i(TAG, course.toString());
+				Log.i(TAG, "ID: "+id);
+				Log.i(TAG, "Date: " + updated_at.toString());
+				Log.i(TAG, "EN: " + en);
+				Log.i(TAG, "FR: " + fr);
+				Log.i(TAG, "ES: " + es);		
+				
+				//TODO Call DB helper function to insert course or update if exists
+				//createCourse(id, en, fr, es, updated_at);
+				
+			} catch (JSONException e) {
+				// Shouldn't happen unless empty response
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void parseProgramme(JSONArray list, int course){
+		JSONObject programme;
+		int id, course_id = course;
+		String en, fr, es, updated_at;
+		for (int i = 0; i < list.length()-1; i++){
+			try {
+				programme = list.getJSONObject(i);
+				id = (Integer) programme.get(ID);				
+				updated_at = programme.get(UPDATED_AT).toString();
+				en = programme.getString(PROGRAMME_NAME_EN);
+				fr = programme.getString(PROGRAMME_NAME_FR);
+				es = programme.getString(PROGRAMME_NAME_ES);
+				course_id = (Integer) programme.get(COURSE_ID);
+				Log.i(TAG, programme.toString());
+				Log.i(TAG, "ID: "+id);
+				Log.i(TAG, "Date: " + updated_at.toString());
+				Log.i(TAG, "EN: " + en);
+				Log.i(TAG, "FR: " + fr);
+				Log.i(TAG, "ES: " + es);		
+				
+				//TODO Call DB helper function to insert course or update if exists
+				//createCourse(id, en, fr, es, updated_at);
+				
+			} catch (JSONException e) {
+				// Shouldn't happen unless empty response
+				e.printStackTrace();
+			}
+		}
 	}
 
 	private void parsePart(JSONArray items){
-		items.toString();
+//		items.toString();
+	}
+	
+	/**
+	 * Actually get all the information from a part. That is to say that the
+	 * full quiz(questions -> options -> feedback), material should be parsed and
+	 * added locally to the database
+	 * @param part
+	 */
+	private void parsePartContent(JSONArray part){
+		
 	}
 
 	public void getCourseList(ProgressDialog progress){
