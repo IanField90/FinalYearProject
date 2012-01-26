9db4b875e6db7ee6fcdd074265c3fad5b2a539b2
Ian Field <ian.field90@gmail.com>
2012-01-26 10:03:52 +0000

    Add further content to encapsulation classes

--- android/src/uk/ac/reading/dp005570/TeachReach/data/Course.java
+++ android/src/uk/ac/reading/dp005570/TeachReach/data/Course.java
@@ -1,5 +1,102 @@
 package uk.ac.reading.dp005570.TeachReach.data;
 
 public class Course {
+	private String mEN, mFR, mES, mUpdated;
+	private int mId;
+	
+	/**
+	 * 
+	 * @param id The ID of the part on server-side
+	 * @param en The English title of the course
+	 * @param fr The French title of the course
+	 * @param es The Spanish title of the course
+	 * @param updated The date the course was last updated on the server
+	 */
+	public Course(int id, String en, String fr, String es, String updated){
+		setId(id);
+		setEN(en);
+		setFR(fr);
+		setES(es);
+		setUpdated(updated);
+	}
 
+	/**
+	 * 
+	 * @return The English title of the course
+	 */
+	public String getEN() {
+		return mEN;
+	}
+
+	/**
+	 * 
+	 * @param mEN The English title of the course
+	 */
+	public void setEN(String mEN) {
+		this.mEN = mEN;
+	}
+
+	/**
+	 * 
+	 * @return The French title of the course
+	 */
+	public String getFR() {
+		return mFR;
+	}
+
+	/**
+	 * 
+	 * @param mFR The French title of the course
+	 */
+	public void setFR(String mFR) {
+		this.mFR = mFR;
+	}
+
+	/**
+	 * 
+	 * @return The Spanish title of the course
+	 */
+	public String getES() {
+		return mES;
+	}
+	
+	/**
+	 * 
+	 * @param mES The Spanish title of the course
+	 */
+	public void setES(String mES) {
+		this.mES = mES;
+	}
+	
+	/**
+	 * 
+	 * @return The date that the course was last updated on the server
+	 */
+	public String getUpdated() {
+		return mUpdated;
+	}
+
+	/**
+	 * 
+	 * @param mUpdated The date that the course was last updated on the server
+	 */
+	public void setUpdated(String mUpdated) {
+		this.mUpdated = mUpdated;
+	}
+
+	/**
+	 * 
+	 * @return The server's ID of the course
+	 */
+	public int getId() {
+		return mId;
+	}
+	
+	/**
+	 * 
+	 * @param mId The server's ID of the course
+	 */
+	public void setId(int mId) {
+		this.mId = mId;
+	}
 }
--- android/src/uk/ac/reading/dp005570/TeachReach/data/Programme.java
+++ android/src/uk/ac/reading/dp005570/TeachReach/data/Programme.java
@@ -1,5 +1,120 @@
 package uk.ac.reading.dp005570.TeachReach.data;
 
 public class Programme {
+	private String mEN, mFR, mES, mUpdated;
+	private int mId, mCourseId;
+	
+	/**
+	 * 
+	 * @param id The ID of the programme on server-side
+	 * @param course_id The ID of the parent Course
+	 * @param en The English title of the programme
+	 * @param fr The French title of the programme
+	 * @param es The Spanish title of the programme
+	 * @param updated The date the programme was last updated on the server
+	 */
+	public Programme(int id, int course_id, String en, String fr, String es, String updated){
+		setId(id);
+		setEN(en);
+		setFR(fr);
+		setES(es);
+		setUpdated(updated);
+		setCourseID(course_id);
+	}
 
+	/**
+	 * 
+	 * @return The English title of the programme
+	 */
+	public String getEN() {
+		return mEN;
+	}
+
+	/**
+	 * 
+	 * @param mEN The English title of the programme
+	 */
+	public void setEN(String mEN) {
+		this.mEN = mEN;
+	}
+
+	/**
+	 * 
+	 * @return The French title of the programme
+	 */
+	public String getFR() {
+		return mFR;
+	}
+
+	/**
+	 * 
+	 * @param mFR The French title of the programme
+	 */
+	public void setFR(String mFR) {
+		this.mFR = mFR;
+	}
+
+	/**
+	 * 
+	 * @return The Spanish title of the programme
+	 */
+	public String getES() {
+		return mES;
+	}
+	
+	/**
+	 * 
+	 * @param mES The Spanish title of the programme
+	 */
+	public void setES(String mES) {
+		this.mES = mES;
+	}
+	
+	/**
+	 * 
+	 * @return The date that the programme was last updated on the server
+	 */
+	public String getUpdated() {
+		return mUpdated;
+	}
+
+	/**
+	 * 
+	 * @param mUpdated The date that the programme was last updated on the server
+	 */
+	public void setUpdated(String mUpdated) {
+		this.mUpdated = mUpdated;
+	}
+
+	/**
+	 * 
+	 * @return The server's ID of the programme
+	 */
+	public int getId() {
+		return mId;
+	}
+	
+	/**
+	 * 
+	 * @param mId The server's ID of the programme
+	 */
+	public void setId(int mId) {
+		this.mId = mId;
+	}
+
+	/**
+	 * 
+	 * @return The parent programme's ID of the programme
+	 */
+	public int getCourseID() {
+		return mCourseId;
+	}
+
+	/**
+	 * 
+	 * @param mProgrammeID The parent programme's ID of the programme
+	 */
+	public void setCourseID(int mCourseID) {
+		this.mCourseId = mCourseID;
+	}
 }
