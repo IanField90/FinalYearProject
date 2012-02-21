0e885a6db51272c823959f7f965235ccee2edee5
Ian Field <ian.field90@gmail.com>
2012-02-21 00:55:11 +0000

    Add some unfinished icons, re-design quiz list, refs #13

--- android/src/uk/ac/reading/dp005570/TeachReach/QuizListActivity.java
+++ android/src/uk/ac/reading/dp005570/TeachReach/QuizListActivity.java
@@ -1,25 +1,18 @@
 package uk.ac.reading.dp005570.TeachReach;
 
-import java.util.ArrayList;
-
-import uk.ac.reading.dp005570.TeachReach.data.QuizStatus;
 import android.app.ListActivity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class QuizListActivity extends ListActivity implements OnItemClickListener{
 	
-	private ArrayList<QuizStatus> mQuizzes = null;
-	private QuizItemAdapter mAdapter;
+//	private ArrayList<QuizStatus> mQuizzes = null;
+	private String[] mQuizzes;
+//	private QuizItemAdapter mAdapter;
 //	private TeachReachPopulater mTeachReachPopulater;
 	
 	@Override
@@ -31,7 +24,7 @@ public class QuizListActivity extends ListActivity implements OnItemClickListene
         setContentView(R.layout.quiz_list);
         getListView().setTextFilterEnabled(false);
         getListView().setOnItemClickListener(this);
-        mQuizzes = new ArrayList<QuizStatus>();
+//        mQuizzes = new ArrayList<QuizStatus>();
 //		int part = getIntent().getIntExtra("Part", 0);
 //		mTeachReachPopulater.get
 //		getIntent().getExtras().getSerializable(key)
@@ -42,57 +35,59 @@ public class QuizListActivity extends ListActivity implements OnItemClickListene
         // TODO ProgressDialog here while retrieving from online if chosen
         populateQuizList();
         
-        this.mAdapter = new QuizItemAdapter(this, R.layout.quiz_item, mQuizzes);
-        setListAdapter(mAdapter);
+//        this.mAdapter = new QuizItemAdapter(this, R.layout.quiz_item, mQuizzes);
+//        setListAdapter(mAdapter);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.quiz_item, mQuizzes)); //TODO Change quiz_item to be suitable to put here
 
 	}
 	
 	
 	private void populateQuizList() {
 		// TODO Future implementation would be here
-        mQuizzes.add(new QuizStatus("Test Quiz 1", false));
-        mQuizzes.add(new QuizStatus("Test Quiz 2", true));		
+//        mQuizzes.add(new QuizStatus("Test Quiz 1", false));
+//        mQuizzes.add(new QuizStatus("Test Quiz 2", true));	
+		mQuizzes = new String[]{ "Test Quiz 1", "Test Quiz 2" };
 	}
-
-	/**
-	 * 
-	 * @author Ian Field
-	 * Class to handle custom list item display
-	 */
-	private class QuizItemAdapter extends ArrayAdapter<QuizStatus>{
-		private ArrayList<QuizStatus> items;
-        
-		public QuizItemAdapter(Context context, int textViewResourceId, ArrayList<QuizStatus> items) {
-            super(context, textViewResourceId, items);
-            this.items = items;
-        }
-		
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-                View v = convertView;
-                if (v == null) {
-                    LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                    v = vi.inflate(R.layout.quiz_item, null);
-                }
-                QuizStatus q = items.get(position);
-                if (q != null) {
-                	TextView title = (TextView) v.findViewById(R.id.quiz_title);
-                	if(title != null){
-                		title.setText(q.getName());
-                	}
-                	
-                	ImageView icon = (ImageView) v.findViewById(R.id.quiz_status);
-                	// TODO tick and cross images instead
-                	if(q.isTaken()){
-                		icon.setImageResource(android.R.drawable.ic_menu_agenda);
-                	}
-                	else{
-                		icon.setImageResource(android.R.drawable.ic_menu_edit);
-                	}
-                }
-                return v;
-        }
-	}
+//
+//	/**
+//	 * 
+//	 * @author Ian Field
+//	 * Class to handle custom list item display
+//	 */
+//	private class QuizItemAdapter extends ArrayAdapter<QuizStatus>{
+//		private ArrayList<QuizStatus> items;
+//        
+//		public QuizItemAdapter(Context context, int textViewResourceId, ArrayList<QuizStatus> items) {
+//            super(context, textViewResourceId, items);
+//            this.items = items;
+//        }
+//		
+//        @Override
+//        public View getView(int position, View convertView, ViewGroup parent) {
+//                View v = convertView;
+//                if (v == null) {
+//                    LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//                    v = vi.inflate(R.layout.quiz_item, null);
+//                }
+//                QuizStatus q = items.get(position);
+//                if (q != null) {
+//                	TextView title = (TextView) v.findViewById(R.id.quiz_title);
+//                	if(title != null){
+//                		title.setText(q.getName());
+//                	}
+//                	
+//                	ImageView icon = (ImageView) v.findViewById(R.id.quiz_status);
+//                	// TODO tick and cross images instead
+//                	if(q.isTaken()){
+//                		icon.setImageResource(android.R.drawable.ic_menu_agenda);
+//                	}
+//                	else{
+//                		icon.setImageResource(android.R.drawable.ic_menu_edit);
+//                	}
+//                }
+//                return v;
+//        }
+//	}
 
 
 //	@Override
